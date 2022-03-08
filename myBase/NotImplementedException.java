a8fc09b2f4d5ccf1578a92f311dde01301c84bbf
mavlyut <334918@niuitmo.ru>
2022-03-09 02:44:46 +0300

    del

--- myBase/NotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package myBase;
-
-public class NotImplementedException extends RuntimeException {
-    public NotImplementedException(String message) {
-        super(String.format("An operation is not implemented: %s", message));
-    }
-}
